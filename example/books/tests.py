c5ab032ae4d60efb22508d71c21a3f493cc6a318
Jason Kraus <jasonk@cukerinteractive.com>
2011-12-06 14:47:04 -0800

    test accessing dims

--- example/books/tests.py
+++ example/books/tests.py
@@ -27,6 +27,8 @@ class BookTestCase(unittest.TestCase):
         book.cover.reprocess_thumbnail_info()
         print book.cover['thumbnail'].width()
         
+        print book.cover.width(), book.cover.height()
+        
         book.cover.reprocess_thumbnails(force_reprocess=True)
     
     def test_admin(self):
